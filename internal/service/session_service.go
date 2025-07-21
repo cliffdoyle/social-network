d4d18203daa7896a0104a18b31ba4675c0f468db
akothouma <lornaakothpb@gmail.com>
2025-07-21 09:46:57 +0300

    add:calling repository method on service layer

--- internal/service/session_service.go
+++ internal/service/session_service.go
@@ -1,13 +1,18 @@
 package service
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/cliffdoyle/social-network/internal/models"
 	"github.com/cliffdoyle/social-network/internal/repository"
+	"github.com/google/uuid"
 )
 
 type SessionService interface {
 	PersistSession(id string) (string, error)
-	GetSessionFromDB(sessionID string) (*models.Sessions,error)
+	GetSessionFromDB(sessionID string) (*models.Sessions, error)
+	DeleteSession(sessionID string)
 }
 
 type sessionService struct {
@@ -19,9 +24,31 @@ func NewSessionService(repo repository.SessionRepository) SessionService {
 }
 
 func (s *sessionService) PersistSession(id string) (string, error) {
-	
+	session := &models.Sessions{
+		SessionID: uuid.New().String(),
+		UserID:    id,
+		Expires:   time.Now().Add(24 * time.Hour),
+	}
+	sessionID, err := s.sessionsRepo.InsertSession(session)
+	if err != nil {
+		return "", err
+	}
+	return sessionID, nil
+
 }
 
-func GetSessionFromDB(sessionID string) (*models.Sessions,error){
+func (s *sessionService) GetSessionFromDB(sessionID string) (*models.Sessions, error) {
+	session, err := s.sessionsRepo.GetSessionID(sessionID)
+	if err != nil {
+		return nil, err
+	}
+	return session, nil
+}
 
+func (s *sessionService) DeleteSession(sessionID string) {
+	err := s.sessionsRepo.DeleteSessionFromDB(sessionID)
+	if err != nil {
+		fmt.Println("failed to delete session.Please try again")
+		return
+	}
 }
