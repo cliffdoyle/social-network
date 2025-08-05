7631bdacd86e2d46f713a4d5337d7e48b04f788f
lakoth <lornaakothpb@gmail.com>
2025-08-05 15:29:24 +0300

    add:defining following services handlers

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/cliffdoyle/social-network/internal/models"
 )
@@ -9,7 +12,7 @@ import (
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	var input models.UserRegistrationRequest
-
+	
 	// Parse JSON from request body
 	err := app.readJSON(w, r, &input)
 	if err != nil {
@@ -37,4 +40,74 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
+}
+
+func (app *application) Follow(w http.ResponseWriter, r *http.Request) {
+	ctx,cancel:=context.WithTimeout(context.Background(),3*time.Second)
+    defer cancel()
+	me:=r.Context().Value("user_uuid").(string)
+	followeeId:=app.readIDParam(r)
+	err:=app.followService.FollowUser(ctx,followeeId,me)
+	if err!=nil{
+		err = app.writeJSON(w, http.StatusInternalServerError, map[string]any{
+		"message": "Failed to exacute follow.Please try again later",
+	})
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+	}
+}
+
+func (app *application) Unfollow(w http.ResponseWriter, r *http.Request) {
+	ctx,cancel:=context.WithTimeout(context.Background(),3*time.Second)
+    defer cancel()
+	me:=r.Context().Value("user_uuid").(string)
+	followeeId:=app.readIDParam(r)
+	err:=app.followService.UnFollowUser(ctx,followeeId,me)
+	if err!=nil{
+		err = app.writeJSON(w, http.StatusInternalServerError, map[string]any{
+		"message": "Failed to exacute unfollow.Please try again later",
+	})
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+	}
+}
+
+func(app *application)Followers(w http.ResponseWriter, r *http.Request){
+	ctx,cancel:=context.WithTimeout(context.Background(),3*time.Second)
+    defer cancel()
+	me:=r.Context().Value("user_uuid").(string)
+    followers,err:=app.followService.ShowFollowers(ctx,me)
+	if err!=nil{
+			err = app.writeJSON(w, http.StatusInternalServerError, map[string]any{
+		"message": "Failed to fetch followers list.Please try again later",
+	})
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+	}
+	json.NewEncoder(w).Encode(map[string]any{
+		"followersList":followers,
+	})
+
+}
+
+func(app *application)Following(w http.ResponseWriter, r *http.Request){
+	ctx,cancel:=context.WithTimeout(context.Background(),3*time.Second)
+    defer cancel()
+	me:=r.Context().Value("user_uuid").(string)
+    following,err:=app.followService.ShowPeopleIFollow(ctx,me)
+	if err!=nil{
+			err = app.writeJSON(w, http.StatusInternalServerError, map[string]any{
+		"message": "Failed to fetch following list.Please try again later",
+	})
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+	}
+	json.NewEncoder(w).Encode(map[string]any{
+		"PeopleIFollow":following,
+	})
+
 }
